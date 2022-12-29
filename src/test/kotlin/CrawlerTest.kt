4546e5e9a584242318cbda0d6904ec83bb23cd33
Andrei.Tyrin <andrei.tyrin@jetbrains.com>
2022-12-29 19:16:22 +0100

    add change tracking test, update actions

--- src/test/kotlin/CrawlerTest.kt
+++ src/test/kotlin/CrawlerTest.kt
@@ -1,12 +1,16 @@
 import crawl.parseActualChanges
+import model.TimeTableChange
+import model.changeTracking
 import notifications.toMessages
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class CrawlerTest {
     @Test
-    fun testParseActualChanges(){
-        val actualChangesMockData = TestResources.getFile("testData/bgprevoz/output.html") ?: throw Exception("Invalid mock html")
+    fun testParseActualChanges() {
+        val actualChangesMockData =
+            TestResources.getFile("testData/bgprevoz/output.html") ?: throw Exception("Invalid mock html")
         val changes = parseActualChanges(actualChangesMockData)
 
         assertTrue(changes.first().routeNumberCell.contains("860J"))
@@ -15,11 +19,63 @@ class CrawlerTest {
 
 
     @Test
-    fun testCreateMessage(){
-        val actualChangesMockData = TestResources.getFile("testData/bgprevoz/output.html") ?: throw Exception("Invalid mock html")
+    fun testCreateMessage() {
+        val actualChangesMockData =
+            TestResources.getFile("testData/bgprevoz/output.html") ?: throw Exception("Invalid mock html")
         val changes = parseActualChanges(actualChangesMockData)
 
-        val prettyPrintedChanges = (changes+changes+changes+changes+changes).toMessages()
+        val prettyPrintedChanges = (changes + changes + changes + changes + changes).toMessages()
         assertTrue(prettyPrintedChanges.size == 3)
+    }
+
+
+    @Test
+    fun testChangeTracking() {
+        val before = setOf(
+            TimeTableChange(
+                routeNumberCell = setOf("1", "2"),
+                changeType = "change",
+                description = "description",
+                dates = "01-02",
+                linkToFullDescription = null
+            ),
+            TimeTableChange(
+                routeNumberCell = setOf("3", "4"),
+                changeType = "change",
+                description = "description 2",
+                dates = "01-02",
+                linkToFullDescription = null
+            )
+        )
+
+        val after = setOf(
+            TimeTableChange(
+                routeNumberCell = setOf("1", "2"),
+                changeType = "change",
+                description = "description",
+                dates = "01-02",
+                linkToFullDescription = null
+            ),
+            TimeTableChange(
+                routeNumberCell = setOf("5", "6"),
+                changeType = "change",
+                description = "description 3",
+                dates = "01-02",
+                linkToFullDescription = null
+            )
+        )
+
+        val new = changeTracking(before, after)
+        assertEquals(
+            setOf(
+                TimeTableChange(
+                    routeNumberCell = setOf("5", "6"),
+                    changeType = "change",
+                    description = "description 3",
+                    dates = "01-02",
+                    linkToFullDescription = null
+                )
+            ), new
+        )
     }
 }
